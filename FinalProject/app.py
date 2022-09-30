80df23fb428079893efd4285c98989d3a92ce0f2
BillieEyelash <riatalwar@hotmail.com>
2022-09-30 12:59:28 -0700

    add python project

--- FinalProject/app.py
+++ FinalProject/app.py
@@ -85,7 +85,50 @@ def resultsForms():
     vehicleInput = request.form.getlist("vehicle")
     return render_template('resultsForms.html', name=inputtedName, sushi=sushiInput, vehicles=vehicleInput)
 
-# HTML/CSS Project
+# HTML/CSS project
 @app.route('/HTMLCSSProject')
 def indexHTMLCSSProject():
    return render_template('indexHTMLCSSProject.html')
+
+# Python project
+@app.route('/PythonProject')
+def indexPythonProject():
+    ''' Description: Launch the home page which contains the form
+        Parameters: None
+        Return: Form page '''
+    genreList = ["Nonfiction", "Realistic Fiction", "Fantasy", "Science Fiction", "Adventure",
+            "Classics", "Mystery", "Horror", "Graphic Novel", "Historical Fiction", "Romance"]
+    genreList.sort()
+    return render_template('indexPythonProject.html', genres=genreList)
+
+@app.route('/PythonProjectResults', methods=['POST'])
+def resultsPythonProject():
+    ''' Description: Launch the results page and retrieve input from form
+        Parameters: None
+        Return: Webpage for form or results '''
+    inputName = request.form.get("firstname"), request.form.get("lastname")
+    passwords = request.form.get("password"), request.form.get("passconfirm")
+    passwordInput = passwords[0]
+    emailInput = request.form.get("email")
+    # Stay at form if input is invalid
+    if not valid_input(inputName, passwords, emailInput):
+        return indexPythonProject()
+
+    genreInput = request.form.getlist("genre")
+    booksReadInput = request.form.get("booksread")
+    return render_template('resultsPythonProject.html', name=inputName, password=passwords[0], email=emailInput, genres=genreInput, booksRead=booksReadInput)
+
+def valid_input(name, passwords, email):
+    ''' Description: Determine whether or not the given inputs are valid
+        Parameters: List [first, last name], List [password, confirm password], String email
+        Return: Boolean name, password, and email are valid '''
+    # Check if name is blank
+    if name[0] == "" or name[1] == "":
+        return False
+    # Check if passwords don't match or are blank
+    elif passwords[0] != passwords[1] or passwords[0] == "":
+        return False
+    # Check if is valid email
+    elif "@" not in email:
+        return False
+    return True
